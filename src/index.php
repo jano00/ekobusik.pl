b25b658ca2fa0afba137b06d2f184ff8fd737364
Janusz Dunia <janusz.dunia@yahoo.com>
2018-04-09 14:29:15 +0200

    video-wrap moved to header section, main banner section deleted, video scaling improved

---
+++ src/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Zwiedzaj z nami Eko-busikami - Kazimierz Dolny</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name='description' content='Oferujemy wycieczki pojazdami elektrycznymi z audio-przewodnikiem po Kazimierzu Dolnym i okolicach. Posiadamy ofertę dla grup zorganizowanych, wycieczek oraz osób indywidualnych. Zapraszamy na przejeżdżkę Eko-busikiem!' />
+    <meta name='keywords' content='' />
+    <!--<link rel="stylesheet" href="../dist/styles/main.css">
+    <link rel="shortcut icon" href="favicon.ico" />-->
+
+</head>
+<body>
+    <form action="send.php" method="POST">
+
+        Client name:
+        <input type="text" name="client" required>
+        <br>
+        Client email:
+        <input type="email" name="email" required>
+        <br>
+        Message:
+        <br>
+        <textarea name="message" rows="10" cols="50" required></textarea>
+        <br>
+        <input type="submit" value="Send message">
+
+    </form>
+</body>
+</html>
